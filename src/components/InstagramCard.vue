d74e932523410d21a11a487a557be03a543ed041
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2020-11-24 23:50:05 +0100

    wk2

---
+++ src/components/InstagramCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card text-left">
+    <div class="card-header">Featured</div>
+    <div class="card-body p-0">
+      <img src="@/assets/photo-moon.jpg" />
+    </div>
+    <div class="card-footer text-muted">2 days ago</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InstagramCard",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
