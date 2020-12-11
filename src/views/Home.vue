e5ad8b81569c08a2f113a559457822b57efdee5b
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2020-12-11 16:50:34 +0100

    search riješen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,16 @@
   <div class="row">
     <div class="col-8">
       <div style="min-width: 100%">
-        <instagram-card v-for="card in cards" :key="card" :info="card" />
+        <instagram-card
+          v-for="card in filteredCards"
+          :key="card"
+          :info="card"
+        />
       </div>
     </div>
     <div class="col-4">
       <div class="card pb-3">
-        <div class="card-body">Mi vam predlažemo </div>
+        <div class="card-body">Mi vam predlažemo</div>
         <insta-predlog v-for="slike in slikeO" :key="slike" :slika="slike" />
       </div>
     </div>
@@ -20,49 +24,56 @@ import store from "@/store";
 import InstagramCard from "../components/InstagramCard.vue";
 import InstaPredlog from "../components/InstaPredlog";
 
-
 let cards = [
-  {"url" : "https://picsum.photos/id/1/800/800",
-  "description" : "laptop",
-  "time" : "about now"
+  {
+    url: "https://picsum.photos/id/1/800/800",
+    description: "laptop",
+    time: "about now",
   },
-  {"url" : "https://picsum.photos/id/2/800/800",
-  "description" : "office",
-  "time" : "few minutes ago"},
-  {"url" : "https://picsum.photos/id/3/800/800",
-  "description" : "working",
-  "time" : "yesterday"}
+  {
+    url: "https://picsum.photos/id/2/800/800",
+    description: "office",
+    time: "few minutes ago",
+  },
+  {
+    url: "https://picsum.photos/id/3/800/800",
+    description: "working",
+    time: "yesterday",
+  },
 ];
 
 let slikeO = [
-  {"url" : "https://picsum.photos/id/1001/300/300",
-  "redni" : "Prva krasna osoba",},
-  {"url" : "https://picsum.photos/id/1004/300/300",
-  "redni" : "Druga krasna osoba"},
-  {"url" : "https://picsum.photos/id/1003/300/300",
-  "redni" : "Treća krasna osoba"}
+  { url: "https://picsum.photos/id/1001/300/300", redni: "Prva krasna osoba" },
+  { url: "https://picsum.photos/id/1004/300/300", redni: "Druga krasna osoba" },
+  { url: "https://picsum.photos/id/1003/300/300", redni: "Treća krasna osoba" },
 ];
 
 export default {
   name: "Home",
   data: function () {
     return {
-      cards: cards,
+      cards,
       store,
       slikeO: slikeO,
     };
   },
   computed: {
-    filteredCards(){
-      let termin = this.store.searchText
-      let newCards = []
-      for (card in this.cards){
-        if (card.description.indexOf(termin) >= 0)
-          {newCards.push(card)}
+    //logika koja cita kartice
+    filteredCards() {
+      
+      let termin = this.store.searchTerm;
+      let newCards = [];
+     
+      for (let card of this.cards) {
+        if (card.description.indexOf(termin) >= 0) {
+          newCards.push(card);
+          console.log(newCards)
+        }
       }
       return newCards;
     },
   },
+
   components: {
     InstagramCard,
     InstaPredlog,
