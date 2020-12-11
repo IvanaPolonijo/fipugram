1abedff5be2a4c1bd367a260cebdd1e33261e442
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2020-12-11 17:57:32 +0100

    firebase u nastajanju

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+//sa https://firebase.google.com/docs/web/setup#using-module-bundlers
