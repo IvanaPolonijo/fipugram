93dae7b3d575ee1d10735bf626480f540d8b1f98
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2020-12-13 22:23:23 +0100

    firebase signup

--- src/firebase.js
+++ src/firebase.js
@@ -6,3 +6,18 @@ import "firebase/auth";
 import "firebase/firestore";
 
 //sa https://firebase.google.com/docs/web/setup#using-module-bundlers
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCUjUxQ7dNi94f9AtfOZD-_nX1QDQ5MyMg",
+    authDomain: "fipugram-a8112.firebaseapp.com",
+    projectId: "fipugram-a8112",
+    storageBucket: "fipugram-a8112.appspot.com",
+    messagingSenderId: "412706339699",
+    appId: "1:412706339699:web:d5b8415ef7ad9cb4cbe306"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export {
+      firebase,
+  };
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,39 +1,100 @@
 <template>
- <div class="about">
- <h1>This is a Sign up page</h1>
- <div class="container">
- <div class="row">
- <div class="col-sm"></div>
- <div class="col-sm">
- <form>
-  <div class="form-group">
- <label for="ime">Ime</label>
- <input type="ime" class="form-control"
-id="ime" placeholder="Ime" />
- </div>
-  <div class="form-group">
- <label for="prezime">Prezime</label>
- <input type="prezime" class="form-control"
-id="prezime" placeholder="Prezime" />
- </div>
- <div class="form-group">
- <label for="exampleInputEmail1">Email address</label>
- <input type="email" class="form-control"
-id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter
-email" />
- <small id="emailHelp" class="form-text text-muted" >We'll
-never share your email with anyone else.</small>
- </div>
- <div class="form-group">
- <label for="exampleInputPassword1">Password</label>
- <input type="password" class="form-control"
-id="exampleInputPassword1" placeholder="Password" />
- </div>
- <button type="submit" class="btn btn-primary">Submit</button>
- </form>
- </div>
- <div class="col-sm"></div>
- </div>
- </div>
- </div>
+  <div class="about">
+    <h1>This is a Sign up page</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm"></div>
+        <div class="col-sm">
+          <form>
+            <div class="form-group">
+              <label for="ime">Ime</label>
+              <input
+                type="ime"
+                v-model="ime"
+                class="form-control"
+                id="ime"
+                placeholder="Ime"
+              />
+            </div>
+            <div class="form-group">
+              <label for="email">Email address</label>
+              <input
+                type="email"
+                v-model="email"
+                class="form-control"
+                id="email"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+              <small id="emailHelp" class="form-text text-muted"
+                >We'll never share your email with anyone else.</small
+              >
+            </div>
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input
+                type="password"
+                v-model="password"
+                class="form-control"
+                id="password"
+                placeholder="Password"
+              />
+            </div>
+            <div class="form-group">
+              <label for="repassword">Repeat password</label>
+              <input
+                type="password"
+                v-model="repassword"
+                class="form-control"
+                id="repassword"
+                placeholder="Repeat Password"
+              />
+            </div>
+            <button type="button" @click="signup" class="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+        <div class="col-sm"></div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import { firebase } from "@/firebase";
+
+export default {
+    name:"SignUp",
+  data() {
+    return {
+      ime: "",
+      email: "",
+      password: "",
+      repassword: "",
+    };
+  },
+ methods: {
+		signup() {
+            if(this.password != this.repassword){
+                alert("The password does not match!");
+            };
+			firebase
+                .auth()
+				.createUserWithEmailAndPassword(this.email, this.password)
+				.then(function () {
+					console.log("Uspješna registracija"); //ovo si još uvijek provjeravam 
+				})
+				.catch(function (error) {
+					console.error("Došlo je do greške: ", error);
+					if (error.message) {
+						alert(error.message);
+					}
+				});
+		},
+	},
+};
+</script>
+
+// Mene ce tu zanimati kako spremati same korisnike...to je tema koja pretpostavljam slijedi 
+// s DB dijelom 
